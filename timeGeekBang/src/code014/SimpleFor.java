f116319d02ea4b552bda38878afc593efff643a1
zhiyangliyang <1219203150@qq.com>
2022-06-02 08:06:25 +0800

    2022/6/1

---
+++ timeGeekBang/src/code014/SimpleFor.java
@@ -0,0 +1,12 @@
+package code014;
+
+/**
+ * 视频20
+ */
+public class SimpleFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i的值是：" + i);
+        }
+    }
+}
