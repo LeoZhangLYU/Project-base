d4dce5f87174c01e1698d9b7b2cb5c3ae0db6e6b
zhiyangliyang <1219203150@qq.com>
2022-06-06 10:27:56 +0800

    从零开始学Java，循环和判断的总结（下）

--- timeGeekBang/src/code018/Comments.java
+++ timeGeekBang/src/code018/Comments.java
@@ -1,5 +1,7 @@
 package code018;
 
+import java.util.Scanner;
+
 /**
  * 生成一个在指定范围内的随机正整数程序关键点
  * 1 得到随机数，java支持得到的0到1的double类型的随机数
@@ -10,31 +12,32 @@ package code018;
  */
 public class Comments {
     public static void main(String[] args) {
-        double random = 0;
+//        创建Scanner来读取用户的键盘输入
+        Scanner in = new Scanner(System.in);
 
-//        要生成一个大于0.5的随机数，只有随机数大于了0.5，循环才会退出。
-        while (random < 0.5) {
-
-            random = Math.random();
-            System.out.println(random);
-        }
-
-        System.out.println("生成的大于0.5的随机数是" + random);
-
+//        游戏设置
         int rangeStart = 30;
-        int rangeEnd = 90;
+        int rangeEnd = 50;
+        int guessTotal = 5;
 
-        int mod = rangeEnd - rangeStart;
+//        游戏统计
+        int totalGameCount = 0;
+        int correctGuessCount = 0;
 
-        if (rangeStart < 0 || rangeEnd < 0) {
-            System.out.println("开始和结束必须是正数或者是零");
-        }
+//        是否结束游戏
+        boolean gameEnd = false;
 
-        if (mod <= 1) {
-            System.out.println("非法的数字范围：(" + rangeStart + "，" + rangeEnd + ")");
-        }
-        for (int i = 0; i < 50; i++) {
+        while (!gameEnd) {
+//            生成指定范围内的随机数
+            int mod = rangeEnd - rangeStart;
 
+            if (rangeStart < 0 || rangeEnd < 0) {
+                System.out.println("开始和结束必须是正数或者是零");
+            }
+
+            if (mod <= 1) {
+                System.out.println("非法的数字范围：(" + rangeStart + "，" + rangeEnd + ")");
+            }
 
 //            random*100为随机生成[0,100)以内的数字
             int bigRandom = (int) (Math.random() * (rangeEnd * 100));
@@ -46,8 +49,42 @@ public class Comments {
             } else if (numberToGuess >= rangeEnd) {
                 numberToGuess = rangeEnd - 1;
             }
+//            剩余的猜测次数
+            int leftToGuess = guessTotal;
 
-            System.out.println("mod=" + mod + ",bigrandom=" + bigRandom + ",numberToGuess=" + numberToGuess);
+            boolean currentGameCounted = false;
+            boolean correctGuess = false;
+
+            System.out.println("请输入猜测的数字，范围在(" + rangeStart + "，" + rangeEnd + ")。输入-1代表结束游戏。");
+            while (leftToGuess > 0) {
+                System.out.print("剩余猜测次数" + leftToGuess + "。请输入本次猜测的数字：");
+                leftToGuess--;
+                int guess = in.nextInt();
+                if (guess <0){
+                    gameEnd = true;
+                    System.out.println("用户选择结束游戏");
+                    break;
+                }
+                if (!currentGameCounted){
+                    totalGameCount++;
+                    currentGameCounted = true;
+                }
+                if (guess > numberToGuess) {
+                    System.out.println("输入的数字比目标数字大");
+                } else if (guess < numberToGuess) {
+                    System.out.println("输入的数字比目标数字小");
+                } else {
+                    correctGuessCount++;
+                    correctGuess = true;
+                    System.out.println("输入正确");
+                    break;
+                }
+            }
+            if (!correctGuess){
+                System.out.println("本次的目标数字是"+numberToGuess);
+            }
+            System.out.println("共进行了" + totalGameCount + "次游戏，其中猜中的次数为" + correctGuessCount);
         }
+
     }
 }
