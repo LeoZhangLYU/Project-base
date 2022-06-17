a4e88c150a3f1bcde95b932e6f507690f672348a
zhiyangliyang <1219203150@qq.com>
2022-06-17 14:27:15 +0800

    通用传统模式简介及通用mapper新增方法

---
+++ Mybatis-Plus入门到熟练/first/src/test/java/com/mp/first/InsertTests.java
@@ -0,0 +1,32 @@
+package com.mp.first;
+
+import com.mp.first.dao.UserMapper;
+import com.mp.first.entity.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class InsertTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void insert(){
+        User user = new User();
+        user.setName("留名强");
+        user.setAge(31);
+        user.setManagerId(1088248166370832385l);
+        user.setCreateTime(LocalDateTime.now());
+        int i = userMapper.insert(user);
+        System.out.println("影像记录数："+i);
+    }
+}
