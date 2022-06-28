b5d408153c0e5983807394fba08647632797f7e0
zhiyangliyang <1219203150@qq.com>
2022-06-28 11:23:08 +0800

    17 React【无人点餐无人收银系统案例】菜品详情请求api渲染数据 以及解析Html【基础项目】-------------bug：无法从axios 获取的值中拿出数据

--- React+React-router4.x+Ant Design+Flux视频教程/reactdemo02/src/components/Pcontent.js
+++ React+React-router4.x+Ant Design+Flux视频教程/reactdemo02/src/components/Pcontent.js
@@ -1,17 +1,59 @@
+import axios from "axios";
+import "../assets/css/pcontent.css";
 import { Component } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 class Pcontent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      list: Object,
+    };
   }
   render() {
     return (
       <>
         <h2>商品详情页面</h2>
+        <Content />
+        {/* <Detail /> */}
       </>
     );
   }
+}
+
+// function Detail() {
+//   const [search, setSearch] = useSearchParams();
+//   const id = search.get("id");
+//   const title = search.get("title");
+//   const content = search.get("content");
+//   return (
+//     <ul>
+//       <li>
+//         <button onClick={() => setSearch("id=008&title=哈哈&content=嘻嘻")}>
+//           点我更新一下收到的search参数
+//         </button>
+//       </li>
+//       <li>消息编号：{id}</li>
+//       <li>消息标题：{title}</li>
+//       <li>消息内容：{content}</li>
+//     </ul>
+//   );
+// }
+let list;
+
+function Content() {
+  let { id } = useParams();
+  let list = "aaaa";
+  axios
+    .get("http://a.itying.com/api/productcontent?id=" + id)
+    .then((response) => {
+      console.log(response.data.result[0]);
+      list = JSON.stringify(response.data.result[0]);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return <p>{list}</p>;
 }
 
 export default Pcontent;
