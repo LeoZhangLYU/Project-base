48b55583668eb2353a37415bd1bd753516902687
zhiyangli <1219203150@qq.com>
2022-10-18 16:43:53 +0800

    极客时间/JAVA基础

---
+++ timeGeekBang/src/code020/WhatlsArray.java
@@ -0,0 +1,12 @@
+package code020;
+
+public class WhatlsArray {
+    public static void main(String[] args) {
+        int[] book = new int[10];
+
+        System.out.println(book[0]);
+        System.out.println(book[1]);
+
+        System.out.println("数组的长度是:"+book.length);
+    }
+}
