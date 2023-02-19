348258bad8bb33a57e5926d30e96887fdcd06c96
zhiyangliyang <1219203150@qq.com>
2023-02-19 14:57:30 +0800

    极客时间/零基础学java  68 | 继承：方便让商品增加新的类别

---
+++ timeGeekBang/src/chapter2/code68/supermarket/HuaweiPhone.java
@@ -0,0 +1,8 @@
+package chapter2.code68.supermarket;
+
+public class HuaweiPhone extends PhoneExtendsMerchandise {
+    public HuaweiPhone(String name, String id, int count, double soldPrice, double purchasePrice, double screenSize,
+                       double cpuHZ, int memoryG, int storageG, String brand, String os) {
+        super(name, id, count, soldPrice, purchasePrice, screenSize, cpuHZ, memoryG, storageG, brand, os);
+    }
+}
