9e3915e2bb735b8c0b508d6aef7b9326dc21459e
zhiyangli <1219203150@qq.com>
2023-05-04 17:00:09 +0800

    Spring Cloud + Vue 前后端分离开发企业级在线视频课程系统  init

---
+++ Spring Cloud + Vue 前后端分离开发企业级在线视频课程系统/SpringCloudAndVue/src/main/java/com/my/springcloudandvue/SpringCloudAndVueApplication.java
@@ -0,0 +1,13 @@
+package com.my.springcloudandvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAndVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAndVueApplication.class, args);
+    }
+
+}
---
+++ Spring Cloud + Vue 前后端分离开发企业级在线视频课程系统/SpringCloudAndVue/src/test/java/com/my/springcloudandvue/SpringCloudAndVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.springcloudandvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAndVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
