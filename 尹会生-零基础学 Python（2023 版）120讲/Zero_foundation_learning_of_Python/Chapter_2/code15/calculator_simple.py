fe070ace6f0c89dbf30823180a3876c09ac99e88
LeoZhangLYU <1219203150@qq.com>
2024-08-21 17:06:15 +0800

    极客时间\零基础学Python：15|小试牛刀：如何制作简单计算器？

---
+++ 尹会生-零基础学 Python（2023 版）120讲/Zero_foundation_learning_of_Python/Chapter_2/code15/calculator_simple.py
@@ -0,0 +1,11 @@
+# 计算两个整数的四则运算计算器
+num1 = input('Enter a number: ')
+num2 = input('Enter another number: ')
+op = input('Enter operator: ')
+result = eval(f'{num1} {op} {num2}')
+print(result)
+
+# NOTE:
+#  程序设计包括：需求分析、代码编写、测试、维护和迭代
+#  文档和搜索引擎对分析问题的作用
+#  写好注释，有助于维护代码
