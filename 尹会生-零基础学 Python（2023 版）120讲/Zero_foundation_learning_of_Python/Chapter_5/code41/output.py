79e30b393102ac1ff1298c289d5fc98f8fc3c731
LeoZhangLYU <1219203150@qq.com>
2024-08-24 13:59:27 +0800

    极客时间\零基础学Python： 41|格式化输出：如何将执行结果通过屏幕输出？

---
+++ 尹会生-零基础学 Python（2023 版）120讲/Zero_foundation_learning_of_Python/Chapter_5/code41/output.py
@@ -0,0 +1,9 @@
+# NOTE: 三种常用的格式化输出方式
+#  1.百分号%
+#  "%s is %s than %s" %("Beautiful", "better", "ugly")
+#  2. format()函数
+#  "{1} is {2} than {o}".format("ugly", "Beautiful", "better")
+#  "{B} is {b} than {u}". format(u="ugly", B= "Beautiful", b= "better")
+#  3.f-strings
+#  u="ugly"; B= "Beautiful"; b= "better"
+#  f"{B} is {b} than {u}"
