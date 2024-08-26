ec3b52c42f1f7b55f1213962f0d003d6a6752d00
LeoZhangLYU <1219203150@qq.com>
2024-08-26 12:51:13 +0800

    极客时间\零基础学Python： 66|模块的导入：如何使用其他人编写好的代码功能？

---
+++ 尹会生-零基础学 Python（2023 版）120讲/Zero_foundation_learning_of_Python/Chapter_8/code66/import.py
@@ -0,0 +1,13 @@
+# NOTE: 导入模块
+#  import os
+#  os.getcwd()
+
+# NOTE: 导入模块的特定方法
+#  from os import listdir, chdir, getcwd
+#  getcwd()
+
+# NOTE: 为导入的模块指定别名
+#  import numpy as np
+
+# NOTE: 导入模块所有函数
+#  from os import *
