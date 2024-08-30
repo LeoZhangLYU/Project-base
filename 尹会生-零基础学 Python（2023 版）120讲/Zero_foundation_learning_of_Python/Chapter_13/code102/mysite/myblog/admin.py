45897f51d03e013f9c2280e8c6583a3d43a011d7
LeoZhangLYU <1219203150@qq.com>
2024-08-30 15:57:02 +0800

    极客时间\零基础学Python： 102|如何使用Django-admin实现文章上传？

---
+++ 尹会生-零基础学 Python（2023 版）120讲/Zero_foundation_learning_of_Python/Chapter_13/code102/mysite/myblog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Article
+
+
+# 设置admin界面展示的样式
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Article)
