8b4a8048db1439455d9c45461208953ac89ddfd0
LeoZhangLYU <1219203150@qq.com>
2024-08-30 17:54:37 +0800

    极客时间\零基础学Python： 103|如何使用Django实现文章发布？

--- 尹会生-零基础学 Python（2023 版）120讲/Zero_foundation_learning_of_Python/Chapter_13/code102/mysite/myblog/views.py
+++ 尹会生-零基础学 Python（2023 版）120讲/Zero_foundation_learning_of_Python/Chapter_13/code102/mysite/myblog/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Article
+
+
 # Create your views here.
+def index(request):
+    # 这里处理模型
+    blog_list = Article.objects.order_by('title')
+    context = {'blog_list': blog_list}
+    return render(request, 'blog.html', context)
